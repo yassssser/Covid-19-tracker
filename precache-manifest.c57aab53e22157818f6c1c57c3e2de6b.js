cd1e7b2800f6c899eafdc9075e58fdc1e2eb3c74
Yasser Benmman <yasserbenmman@gmail.com>
2020-04-11 18:59:16 +0000

    Updates

---
+++ precache-manifest.c57aab53e22157818f6c1c57c3e2de6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e3d44b81369fa797795f21dd0107cf",
+    "url": "/Covid-19-tracker/index.html"
+  },
+  {
+    "revision": "dbe7af27426682f0c82f",
+    "url": "/Covid-19-tracker/static/css/main.11fbdc5c.chunk.css"
+  },
+  {
+    "revision": "f6d0f49c5b9eca937ad4",
+    "url": "/Covid-19-tracker/static/js/2.9368cb30.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-tracker/static/js/2.9368cb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe7af27426682f0c82f",
+    "url": "/Covid-19-tracker/static/js/main.cfc1e315.chunk.js"
+  },
+  {
+    "revision": "0d32d0607021dfe04b8f",
+    "url": "/Covid-19-tracker/static/js/runtime-main.ba535b33.js"
+  },
+  {
+    "revision": "b71b7d6046bdfbee99ed17e1c3adecdf",
+    "url": "/Covid-19-tracker/static/media/loading.b71b7d60.gif"
+  },
+  {
+    "revision": "30b22443b4f26bc8124fa510eba0b07e",
+    "url": "/Covid-19-tracker/static/media/merci.30b22443.jpeg"
+  }
+]);
